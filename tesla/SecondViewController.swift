1377a91cfdb89350f4ff4420142bb8049e697e2f
Sonar <Sonar@imacs-imac.local>
2018-12-12 19:37:06 -0500

    Initial Commit

---
+++ tesla/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  tesla
+//
+//  Created by Sonar on 12/12/18.
+//  Copyright © 2018 Sonar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
